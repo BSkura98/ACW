9572510bac1f4c1aaf900a2f9c926632255d6faf
student <bartek33s@wp.pl>
2022-11-19 14:13:44 +0100

    Add query parameters handling

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -3,6 +3,8 @@ import http.server
 import socketserver
 import os
 
+from urllib.parse import urlparse, parse_qs
+
 #print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
@@ -10,13 +12,22 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
+        parse = urlparse(self.path)
         
-        if self.path == '/':
+        if parse.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.end_headers()
+            
+            query_params = parse_qs(parse.query)
+            
+            if query_params.get('cmd', None) == ['time']:            
+            	self.wfile.write(b"time\n")
+            elif query_params.get('cmd', None) == ['rev']:
+            	self.wfile.write(b"rev\n")
+            else:
+            	self.wfile.write(b"Hello World!\n")
         else:
             super().do_GET()
     
