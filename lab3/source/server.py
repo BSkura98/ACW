1acc31f759e1eba2f6c2ade28b10312b57158da7
student <bartek33s@wp.pl>
2022-11-26 11:01:17 +0100

    Add str handling

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -3,6 +3,7 @@ import http.server
 import socketserver
 import os
 import json
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -11,13 +12,21 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
+        
+        url_parsed = urlparse(self.path)
+        params = parse_qs(url_parsed.query)
 
-        if self.path == '/':
+        if url_parsed.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header('Content-type', 'application/json; charset=UTF-8')
             self.end_headers()            
             self.wfile.write(str.encode(json.dumps({'lowerCase': 1, 'uppercase': 1, 'digits': 1, 'special': 1})))
+            
+            if params.get('str', None):
+                text = params.get('str', None)[0]
+                self.wfile.write(str.encode(f'{text}\n'))
+            
         else:
             super().do_GET()
 
